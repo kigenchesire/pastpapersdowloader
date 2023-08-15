3271e2eec7cf9108dbc229325bd3ddc06af36e52
kigen chesire <ekchesire10@gmail.com>
2023-08-15 08:25:57 +0300

    add new view

--- config/app.php
+++ config/app.php
@@ -57,7 +57,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL'),
+    'asset_url' => env('http://arcane-atoll-38405-e89672b8becb.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
