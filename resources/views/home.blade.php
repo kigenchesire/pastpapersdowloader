f4f6dbeead59bd0cab0f700364036d1fa701cc27
kigen chesire <ekchesire10@gmail.com>
2023-08-26 23:49:57 +0300

    adding dummy data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,5 @@
 <div class="v6_47"></div><div class="v6_48"></div><span class="v6_49">Semester 1.2 Units:</span>
 <div class="v6_50"></div><div class="v6_51"></div><span class="v6_52">Semester 2.1 Units:</span>
 <div class="v6_53"></div><div class="v6_54"></div><span class="v6_55">Semester 2.2 Units:</span>
-<div class="v6_56"></div><div class="v6_57"></div><span class="v6_58">Semester 1.1 Units:</span>
-<div class="v6_59"></div><div class="v6_60"></div><span class="v6_61">Semester 3.1 Units:</span>
-<div class="v6_62"></div><div class="v6_63"></div><span class="v6_64">Semester 1.1 Units:</span>
-<div class="v6_65"></div><div class="v6_66"></div><span class="v6_67">Semester 3.2 Units:</span>
+
 <div class="v6_68"></div></div></body>
